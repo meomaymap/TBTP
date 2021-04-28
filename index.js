a151b957fac7a7cf69b3e3311b4bc3f29736c3bf
meomaymap <79776624+meomaymap@users.noreply.github.com>
2021-04-28 22:26:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ if (typeof(Storage) !== "undefined") {
   //document.getElementById("result").innerHTML = localStorage.getItem("lastname");
   
   var p = localStorage.getItem("AIzaSyC7om8zq3wvGcrhu5PyZlZoi2QrNmkmPJI._p");
+  var e = localStorage.getItem("AIzaSyC7om8zq3wvGcrhu5PyZlZoi2QrNmkmPJI._e");
+  var n = localStorage.getItem("AIzaSyC7om8zq3wvGcrhu5PyZlZoi2QrNmkmPJI._n");
+  var i = localStorage.getItem("AIzaSyC7om8zq3wvGcrhu5PyZlZoi2QrNmkmPJI._i");
 
   if (p){
     location.replace("https://nvq-auth.stackblitz.io");
-    
   } else {
-    location.replace("https://firebase-meeting-quang.stackblitz.io");
+    if (e){
+      location.replace("https://meomaymap.github.io/BDTT_TBTP/test2.html");
+    } else {
+      location.replace("https://firebase-meeting-quang.stackblitz.io");
+    }
   }
 } else {
   //document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
