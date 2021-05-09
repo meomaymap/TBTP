9b98b46014afea354d79ca143e6a359c8a4f4b88
meomaymap <79776624+meomaymap@users.noreply.github.com>
2021-05-09 16:45:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,26 @@
-//Định nghĩa viết tắt cho element
-   function ECN(NameClass, pos) {return document.getElementsByClassName(NameClass)[pos];}
-   function ECN_Array(NameClass, pos) {return document.getElementsByClassName(NameClass);}
-   function ETN(NameTag, pos) {return document.getElementsByTagName(NameTag)[pos];}
-   function ETN_Array(NameTag, pos) {return document.getElementsByTagName(NameTag);}
-   function EN(Name, pos) {return document.getElementsByName(Name)[pos];}
-   function EN_Array(Name, pos) {return document.getElementsByName(Name);}
-   function EID(NameID) {return document.getElementById(NameID);}
-//Kết thúc Định nghĩa viết tắt cho element
+ //Disable get code
+        //DISABLE RIGHT-CLICK
+        document.addEventListener("contextmenu", function(e){
+            e.preventDefault();
+        }, false);
 
-//Cài đặt properties cho element
-   function setStyle(el, propertyObject ) {
-      for (var property in propertyObject) {
-         el.style[property] = propertyObject[property];
-      }
-   }
-   function setStyleAllClass(NameClass, propertyObject) {
-      var el = ECN_Array(NameClass)
-      for (i=0; i<el.length;i++) {
-         setStyle(el[i], propertyObject);
-      }
-   }
-//Kết thúc Cài đặt properties cho element        
-
-//Tạo thẻ link bằng js
-   function loadjscssfile(filename, filetype){
-      if (filetype=="js"){ //if filename is a external JavaScript file
-         var fileref=document.createElement('script')
-         fileref.setAttribute("type","text/javascript")
-         fileref.setAttribute("src", filename)
-      }
-            else if (filetype=="css"){ //if filename is an external CSS file
-                var fileref=document.createElement("link")
-                fileref.setAttribute("rel", "stylesheet")
-                fileref.setAttribute("type", "text/css")
-                fileref.setAttribute("href", filename)
+        //DISABLE “VIEW SOURCE” SHORTCUT KEY
+        document.addEventListener("keydown", function(e){
+        // USE THIS TO DISABLE CONTROL AND ALL FUNCTION KEYS
+        // if (e.ctrlKey || (e.keyCode>=112 && e.keyCode<=123)) {
+        // THIS WILL ONLY DISABLE CONTROL AND F12
+            if (e.ctrlKey || e.keyCode==123) {
+                e.stopPropagation();
+                e.preventDefault();
             }
-            if (typeof fileref!="undefined")
-                document.getElementsByTagName("head")[0].appendChild(fileref)
-        }
-
+        });
 
 function preventPullToRefresh(element) {
-
-			var prevent = false;
-
-			document.querySelector(element).addEventListener('touchstart', function(e){
-
-                if (e.touches.length !== 1) { 
-                    
-                   
-                    return; 
-                }
+	var prevent = false;
+	document.querySelector(element).addEventListener('touchstart', function(e){
+	if (e.touches.length !== 1) { 
+		return; 
+	}
 
                 
 
