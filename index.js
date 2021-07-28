3f54bb2a8111655504aa5c938fc745383e4d1411
meomaymap <79776624+meomaymap@users.noreply.github.com>
2021-07-28 07:30:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -109,3 +109,15 @@ var eventMethod = window.addEventListener
 //function myFunction(event) {
 //  alert(localStorage.getItem("QQQ"));
 //}
+
+
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
